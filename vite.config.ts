c6658bfc3b76c63ecf52965eb3499d8830740787
ohadren-source <ohadren@gmail.com>
2026-03-26 22:51:58 -0400

    saucier

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       'cats-up.fun'
     ],
     host: true,
-    port: Number(process.env.PORT) || 3000
+    port: Number(process.env.PORT) || 3000,
+    strictPort: true
   }
 })
